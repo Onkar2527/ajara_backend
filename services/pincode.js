80191e5cc9f5ae804f98d769f222f0a5007014a6
silentprasad <prasadshete112@gmail.com>
2023-09-14 06:22:30 +0000

    ok

--- services/pincode.js
+++ services/pincode.js
@@ -1,87 +1,83 @@
 const db = require('../utilities/dbModule')
 
-exports.getState = (req, res) =>{
+exports.getState = (req, res) => {
     let supportKey = req.headers['supportkey'];
-    db.executeQueryData(`select distinct STATE from pincode_master where COUNTRY = 'India'`, '', supportKey , (error, result)=>{
-        if(error){
-            console.log("error",error);
-            result.send({
+    db.executeQueryData(`select distinct STATE from pincode_master where COUNTRY = 'India' order by STATE asc`, '', supportKey, (error, result) => {
+        if (error) {
+            console.log("error", error);
+            res.send({
                 "code": 400,
                 "message": "Failed"
             })
         }
-        else
-        {
+        else {
             res.send({
                 "code": 200,
                 "message": "succeess",
-                "data" : result
+                "data": result
             })
         }
     })
 
 }
 
-exports.getDistrict = (req, res) =>{
+exports.getDistrict = (req, res) => {
     let supportKey = req.headers['supportkey'];
-    db.executeQueryData(`select distinct DISTRICT from pincode_master where 1 ${req.body.filter}`, '', supportKey , (error, result)=>{
-        if(error){
-            console.log("error",error);
-            result.send({
+    db.executeQueryData(`select distinct DISTRICT from pincode_master where 1 AND  STATE = '${req.body.filter}' order by DISTRICT ASC`, '', supportKey, (error, result) => {
+        if (error) {
+            console.log("error", error);
+            res.send({
                 "code": 400,
                 "message": "Failed"
             })
         }
-        else
-        {
+        else {
             res.send({
                 "code": 200,
                 "message": "succeess",
-                "data" : result
+                "data": result
             })
         }
     })
 
 }
 
-exports.getTaluka = (req, res) =>{
+exports.getTaluka = (req, res) => {
     let supportKey = req.headers['supportkey'];
-    db.executeQueryData(`select distinct TALUKA from pincode_master where 1 ${req.body.filter}`, '', supportKey , (error, result)=>{
-        if(error){
-            console.log("error",error);
-            result.send({
+    db.executeQueryData(`select distinct TALUKA from pincode_master where 1 AND DISTRICT = '${req.body.filter}' order by TALUKA asc`, '', supportKey, (error, result) => {
+        if (error) {
+            console.log("error", error);
+            res.send({
                 "code": 400,
                 "message": "Failed"
             })
         }
-        else
-        {
+        else {
             res.send({
                 "code": 200,
                 "message": "succeess",
-                "data" : result
+                "data": result
             })
         }
     })
 
 }
 
-exports.getVillage = (req, res) =>{
+exports.getVillage = (req, res) => {
     let supportKey = req.headers['supportkey'];
-    db.executeQueryData(`select VILLAGE from pincode_master where 1 ${req.body.filter}`, '', supportKey , (error, result)=>{
-        if(error){
-            console.log("error",error);
-            result.send({
+    db.executeQueryData(`select distinct VILLAGE from pincode_master where 1 AND TALUKA = '${req.body.filter}' order by VILLAGE asc`, '', supportKey, (error, result) => {
+        if (error) {
+            console.log("error", error);
+            res.send({
                 "code": 400,
                 "message": "Failed"
             })
         }
-        else
-        {
+        else {
             res.send({
                 "code": 200,
                 "message": "succeess",
-                "data" : result
+                "data": result
             })
         }
     })
