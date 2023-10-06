ac4da549515f20ada1213cce10dfa121d1c6536a
lukmansayyad <you@example.com>
2023-10-06 17:51:17 +0530

    fixed spelling issue in voterVerification.js

--- router/voterIdVerification.js
+++ router/voterIdVerification.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const voterIdVerificationService = require('../services/voterIdVerification')
+const voterIdVerificationService = require('../services/VoterIdVerification')
 
 router 
     .post('/get', voterIdVerificationService.get)
