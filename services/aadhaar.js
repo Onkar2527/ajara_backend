36b2c77afa8f55158109b6b174c4df4d0e42f21e
lukmansayyad <you@example.com>
2023-10-07 16:43:42 +0530

    solved a bug

--- services/aadhaar.js
+++ services/aadhaar.js
@@ -1,13 +1,13 @@
 
 const db = require('../utilities/dbModule');
 
-function reqData(req){
+function reqData(req) {
     const data = {
         AADHAAR_NUMBER: req.body.AADHAAR_NUMBER,
         APPLICANT_ID: req.body.APPLICANT_ID,
         APPLICANT_NO: req.body.APPLICANT_NO,
         ADDRESS_ID: req.body.ADDRESS_ID,
-       
+
         DOB: req.body.DOB,
         APPLICANT_FULL_NAME: req.body.APPLICANT_FULL_NAME,
         GENDER: req.body.GENDER,
@@ -18,8 +18,8 @@ function reqData(req){
 
 
 exports.create = (req, res) => {
-   let data = reqData(req)
- let supportKey  = req.headers['supportKey']
+    let data = reqData(req)
+    let supportKey = req.headers['supportKey']
     var setData = "";
     var recordData = [];
 
@@ -31,11 +31,10 @@ exports.create = (req, res) => {
         recordData.push(data[key]);
     });
 
-   
-console.log("data from req", data);
-    db.executeDML(`insert into aadhaar_address set ?`, data.ADDRESS_ID , supportKey ,con, (error, results)=>{
-        if(error)
-        {
+
+    console.log("data from req", data);
+    db.executeDML(`insert into aadhaar_address set ?`, data.ADDRESS_ID, supportKey, con, (error, results) => {
+        if (error) {
             db.rollbackConnection(con)
             console.log("error", error);
             res.send({
@@ -45,11 +44,11 @@ console.log("data from req", data);
 
 
         }
-        else{
+        else {
             data.ADDRESS_ID = results.insertId;
             console.log("insertid", results);
             console.log("data at the end", data);
-            db.executeDML(`insert into aadhaar_verified_list set ?`, data, supportKey, con , (error) => {
+            db.executeDML(`insert into aadhaar_verified_list set ?`, data, supportKey, con, (error) => {
                 if (error) {
                     console.log("error", error);
                     db.rollbackConnection(con)
@@ -76,8 +75,14 @@ console.log("data from req", data);
 
 exports.get = (req, res) => {
     let supportKey = req.headers['supportkey'];
-    const q = `select * from aadhaar_verified_list where APPLICANT_NO = ${req.body.APPLICANT_NO} AND AADHAAR_NUMBER = ${req.body.AADHAAR_NUMBER} `
-   console.log(q);
+    let q = ``;
+    if (req.body.AADHAAR_NUMBER)
+        q = `select * from aadhaar_verified_list where APPLICANT_NO = ${req.body.APPLICANT_NO} AND AADHAAR_NUMBER = ${req.body.AADHAAR_NUMBER} `
+    else{
+        q =`select * from aadhaar_verified_list where 0`
+    }
+
+    
     db.executeQuery(q, supportKey, (error, results) => {
         if (error) {
             console.log("err", error);
@@ -88,24 +93,24 @@ exports.get = (req, res) => {
         }
         else {
             if (results.length > 0) {
-               
-                    db.executeQuery(`select * from aadhaar_address where ID = ${results[0].ADDRESS_ID}`, supportKey, (error, resultAdress) => {
-                        if (error) {
-                            console.log("error", error);
-                            res.send({"msg":"failed"})
-                        }
-                        else {
-                            results[0].ADDRESS_ID = resultAdress
-                            res.send({
-                                "code": 200,
-                                "message": "OK",
-                                "data": results
 
-                            })
+                db.executeQuery(`select * from aadhaar_address where ID = ${results[0].ADDRESS_ID}`, supportKey, (error, resultAdress) => {
+                    if (error) {
+                        console.log("error", error);
+                        res.send({ "msg": "failed" })
+                    }
+                    else {
+                        results[0].ADDRESS_ID = resultAdress
+                        res.send({
+                            "code": 200,
+                            "message": "OK",
+                            "data": results
 
-                        }
-                    })
-               
+                        })
+
+                    }
+                })
+
             }
             else {
                 res.send({
