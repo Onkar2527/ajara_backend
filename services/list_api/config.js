049cfc26204fc56423bde3065eeadf5e387c1a0d
lukmansayyad <luqmansayyad8@gmail.com>
2024-01-22 17:19:05 +0530

    change configaration

--- services/list_api/config.js
+++ services/list_api/config.js
@@ -1,7 +1,6 @@
 exports.config = {
     mode: "local",
-    production: {},
-    local: {
+    production: {
         database_config: {
             user:'root',
             database_type: "mysql",
@@ -23,5 +22,48 @@ exports.config = {
             ]
         }
     },
-    testing_server: {}
+    local: {
+        database_config: {
+            user:'root',
+            database_type: "mysql",
+            host: "localhost",
+            port: "",
+            database_name: "list_cbs_masters",
+            ispass: true,
+            password: 'No.5670@'
+        },
+        api: {
+            host: "http://172.100.30.89",
+            port: "8888",
+            logs: true,
+            routes: [
+                { name: 'jwtToken', url: '/CustomerInfo/api/auth/getJwt', operation: 'get' },
+                { name: 'masters', url: '/MasterLOV/customer/getMasterLOV/', operation: 'get' },
+                { name: 'getCustomer', url: '/CustomerInfo/customer/getCustomerInfo?', operation: 'get' },
+                { name: 'onBoard', url: '/OnBoardCustomer/customer/onBoard', operation: 'post' }
+            ]
+        }
+    },
+    testing_server: {
+        database_config: {
+            user:'root',
+            database_type: "mysql",
+            host: "localhost",
+            port: "",
+            database_name: "list_cbs_masters",
+            ispass: true,
+            password: 'Sangli123#'
+        },
+        api: {
+            host: "http://172.100.30.89",
+            port: "8888",
+            logs: true,
+            routes: [
+                { name: 'jwtToken', url: '/CustomerInfo/api/auth/getJwt', operation: 'get' },
+                { name: 'masters', url: '/MasterLOV/customer/getMasterLOV/', operation: 'get' },
+                { name: 'getCustomer', url: '/CustomerInfo/customer/getCustomerInfo?', operation: 'get' },
+                { name: 'onBoard', url: '/OnBoardCustomer/customer/onBoard', operation: 'post' }
+            ]
+        }
+    }
 }
